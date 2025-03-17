266c948eae5ad232803c3b61673d1037958641af
ilyalyai <lyapilya@yandex.ru>
2025-03-17 22:20:35 +0300

    Тестовые Razor Pages странички

--- TransportOrderApp/Pages/Orders/Details.cshtml.cs
+++ TransportOrderApp/Pages/Orders/Details.cshtml.cs
@@ -1,3 +1,5 @@
+using TransportOrderApp.Models;
+using TransportOrderApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,23 @@ namespace TransportOrderApp.Pages.Orders
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IOrderService _orderService;
+
+        public DetailsModel(IOrderService orderService)
         {
+            _orderService = orderService;
+        }
+
+        public Order Order { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            Order = _orderService.GetOrderById(id);
+            if (Order == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
     }
 }
--- TransportOrderApp/Pages/Orders/Index.cshtml.cs
+++ TransportOrderApp/Pages/Orders/Index.cshtml.cs
@@ -1,12 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
+using TransportOrderApp.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TransportOrderApp.Models;
 
 namespace TransportOrderApp.Pages.Orders
 {
     public class IndexModel : PageModel
     {
+        private readonly IOrderService _orderService;
+
+        public IndexModel(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public List<Order> Orders { get; set; }
+
         public void OnGet()
         {
+            Orders = _orderService.GetOrders();
         }
     }
 }
